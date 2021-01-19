e2de5fce972243112c35d04373aefba0928f45ac
leon Boakye <33208650+leonfiasco@users.noreply.github.com>
2021-01-19 22:39:11 +0000

    Add files via upload

---
+++ Forecast.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from '../components/Card';
+
+import '../styles/Forecast.css';
+
+function Forecast({ forecast = [] }) {
+    const arr = forecast.list;
+    return (
+        <div className='weather-list'>
+            {arr && arr.map((el, i) => {
+              return  i % 8 === 0 ? <Card key={i} date={el.dt_txt} /> : null
+            }) }
+        </div>
+    )
+
+}
+
+export default Forecast

8fbe6e63c2577cc7ac76d1428027f5a300cd4938
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-01-19 17:19:18 +0000

    created a forecast custom hook and pass it to the forecast component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -3,10 +3,18 @@ import React from 'react';
 import '../styles/Forecast.css';
 
 function Forecast({ forecast }) {
-    console.log(forecast);
+    const fiveDay = () => {
+        for (let i = 0; i < forecast.list.length; i+= 8) {
+            return forecast.list[i];
+        }
+     }
+
+     console.log(fiveDay);
     return (
         <div className='weather-list'>
-            
+            {
+             
+            }
         </div>
     )
 }
